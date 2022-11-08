a2869f837fc806689f6ff8a23ec040102d356efb
saltchess <saltrainchess@gmail.com>
2022-11-08 23:41:32 +0800

    revise

--- daily_clock.js
+++ daily_clock.js
@@ -5,27 +5,32 @@ const axios = require('axios').create({
 	headers: { "Content-Type": "application/x-www-form-urlencoded" },
 })
 function preCheck(res) {
-	if (res.status != 200) throw new Error()
+	if (res.status != 200) throw res.statusText
 	return res.data
+}
+function errorCode(data) {
+	if (data.e != 0) throw data.m
+	return data
+}
+function sweep(err) {
+	console.error(err)
+	process.exit(1)
 }
 
 function login(data) {
 	return axios.post("/uc/wap/login/check", data).then(res => {
 		axios.defaults.headers.Cookie = res.headers["set-cookie"]
 		return res
-	}).then(preCheck).then(data => {
-		if (![0, 1].includes(data.e)) throw new Error()
-		return data
-	})
+	}).then(preCheck).then(errorCode).catch(sweep)
 }
 function redirect() {
-	return axios.get("/buaaxsncov/wap/default/get-info").then(preCheck)
+	return axios.get("/buaaxsncov/wap/default/get-info").then(preCheck).catch(sweep)
 }
 function clock(data) {
-	return axios.post("/buaaxsncov/wap/default/save", data).then(preCheck)
+	return axios.post("/buaaxsncov/wap/default/save", data).then(preCheck).then(errorCode).catch(sweep)
 }
 function logout() {
-	return axios.get("/uc/wap/login/logout").then(preCheck)
+	return axios.get("/uc/wap/login/logout").then(preCheck).catch(sweep)
 }
 
 (async () => {
@@ -35,36 +40,30 @@ function logout() {
 	let tmpl = (await redirect()).d
 	console.log("Redirect Success!");
 
-	// let form = Object.assign(
-	// 	{},
-	// 	tmpl.oldInfo,
-	// 	tmpl.info,
-	// 	{ realname: "颜宇琦", number: "19374307", created: Math.floor(Date.now() / 1000), date: tmpl.date.replace("-", ""), id: "?" }
-	// )
 	let form = Object.assign(
 		{
-			"sfzs": "",
-			"bzxyy": "", "bzxyy_other": "",
-			"brsfzc": "1", "tw": "", "sfcxzz": "", "zdjg": "",
+			/*"sfzs": "",
+			"bzxyy": "", "bzxyy_other": "",*/
+			"brsfzc": "1",/* "tw": "", "sfcxzz": "", "zdjg": "",
 			"zdjg_other": "", "sfgl": "", "gldd": "",
 			"gldd_other": "", "glyy": "", "glyy_other": "",
-			"gl_start": "", "gl_end": "", "sfmqjc": "",
-			"sfzc_14": "1", "sfqw_14": "", "sfqw_14_remark": "",
+			"gl_start": "", "gl_end": "", "sfmqjc": "",*/
+			"sfzc_14": "1", /*"sfqw_14": "", "sfqw_14_remark": "",
 			"sfzgfx": "", "sfzgfx_remark": "",
 			"sfjc_14": "", "sfjc_14_remark": "", "sfjcqz_14": "",
 			"sfjcqz_14_remark": "", "sfgtjz_14": "",
-			"sfgtjz_14_remark": "", "szsqqz": "", "sfyqk": "",
-			"szdd": "1", "area": "",
+			"sfgtjz_14_remark": "", "szsqqz": "", "sfyqk": "",*/
+			"szdd": "1", /*"area": "",
 			"city": "", "province": "",
 			"address": "",
 			"gwdz": "", "is_move": "", "move_reason": "", "move_remark": "",
 			"realname": "", "number": "", "uid": "", "created": "",
-			"date": "", "id": ""
+			"date": "", "id": ""*/
 		},
 		{
 			"sfzs": "1", "area": "北京市 海淀区",
 			"city": "北京市", "province": "北京市",
-			"address": "北京市海淀区花园路街道北京航空航天大学大运村学生公寓5号楼"
+			"address": "北京市海淀区花园路街道聚湘园北京航空航天大学学院路校区"
 		},
 		{
 			"realname": tmpl.uinfo.realname,
@@ -77,7 +76,7 @@ function logout() {
 			"id": tmpl.info.id
 		}
 	)
-	console.log((await clock(form)))
+	await clock(form)
 	console.log("Clock Success!");
 
 	await logout()
