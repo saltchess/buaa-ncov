5fa497037462142994c23b20147359cab8e46d41
saltchess <saltrainchess@gmail.com>
2022-11-12 16:06:57 +0800

    again

--- daily_clock.js
+++ daily_clock.js
@@ -4,49 +4,47 @@ const axios = require('axios').create({
 	baseURL: 'https://app.buaa.edu.cn/',
 	headers: { "Content-Type": "application/x-www-form-urlencoded" },
 })
-function preCheck(res) {
-	if (res.status != 200) throw res.statusText
+axios.interceptors.request.use(res => {
+	axios.defaults.headers.Cookie ??= res.headers["set-cookie"]
+	if (res.status != 200) return Promise.reject(res.statusText)
 	return res.data
-}
+})
+
 function errorCode(data) {
 	if (data.e != 0) throw data.m
 	return data
 }
-function sweep(err) {
-	console.error(err)
-	process.exit(1)
-}
-
 function login(data) {
-	return axios.post("/uc/wap/login/check", data).then(res => {
-		axios.defaults.headers.Cookie = res.headers["set-cookie"]
-		return res
-	}).then(preCheck).then(errorCode).catch(sweep)
+	return axios.post("/uc/wap/login/check", data).then(errorCode)
 }
 function redirect() {
-	return axios.get("/buaaxsncov/wap/default/get-info").then(preCheck).catch(sweep)
+	return axios.get("/buaaxsncov/wap/default/get-info")
 }
 function clock(data) {
-	return axios.post("/buaaxsncov/wap/default/save", data).then(preCheck).then(errorCode).catch(sweep)
+	return axios.post("/buaaxsncov/wap/default/save", data).then(errorCode)
 }
 function logout() {
-	return axios.get("/uc/wap/login/logout").then(preCheck).catch(sweep)
+	return axios.get("/uc/wap/login/logout")
 }
 
 (async () => {
-	await login({ ...argv })
-	console.log("Login Success!");
+	try {
+		await login({ ...argv })
+		console.log("Login Success!");
 
-	let tmpl = (await redirect()).d
-	console.log("Redirect Success!");
+		let { d: tmpl } = await redirect()
+		console.log("Redirect Success!");
 
-	let form = Object.assign(
-		tmpl.oldInfo,
-		{ realname: tmpl.uinfo.realname, number: tmpl.uinfo.role.number },
-	)
-	await clock(form)
-	console.log("Clock Success!");
+		await clock(Object.assign(
+			tmpl.oldInfo,
+			{ realname: tmpl.uinfo.realname, number: tmpl.uinfo.role.number },
+		))
+		console.log("Clock Success!");
 
-	await logout()
-	console.log("Logout Success!");
+		await logout()
+		console.log("Logout Success!");
+	} catch (err) {
+		console.error(err)
+		process.exit(1)
+	}
 })()
